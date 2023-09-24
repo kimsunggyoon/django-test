7949a1df3e7623a42eb63bee5c155adfcba5d8e9
sunggyoon <tjdrbs1011@gmail.com>
2023-09-24 14:35:43 +0900

    order project create

---
+++ order/user_order/views.py
@@ -0,0 +1,65 @@
+from rest_framework import viewsets,status
+from .serializers import ShopSerializer,OrderSerializer
+from .models import Shop,Order
+from rest_framework.response import Response
+
+class ShopViewSet(viewsets.ViewSet):
+    def list(self,request):
+        shops = Shop.objects.all()
+        serializer = ShopSerializer(shops , many=True)
+        return Response(serializer.data)
+
+    def create(self,request):
+        serializer = ShopSerializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        
+        return Response(serializer.data,status= status.HTTP_201_CREATED)
+
+    def retrieve(self,request,pk=None):
+        shop = Shop.objects.get(id=pk)
+        serializer = ShopSerializer(shop)
+        return Response(serializer.data)
+
+    def update(self,request,pk=None):
+        shop = Shop.objects.get(id=pk)
+        serializer = ShopSerializer(instance=shop, data = request.data)
+        serializer.is_vaild(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+
+    def destroy(self,request,pk=None):
+        shop = Shop.objects.get(id=pk)
+        shop.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class OrderViewSet(viewsets.ViewSet):
+    def list(self,request):
+        orders = Order.objects.all()
+        serializer = OrderSerializer(orders , many=True)
+        return Response(serializer.data)
+
+    def create(self,request):
+        serializer = OrderSerializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        
+        return Response(serializer.data,status= status.HTTP_201_CREATED)
+
+    def retrieve(self,request,pk=None):
+        order = Order.objects.get(id=pk)
+        serializer = OrderSerializer(order)
+        return Response(serializer.data)
+
+    def update(self,request,pk=None):
+        order = Order.objects.get(id=pk)
+        serializer = OrderSerializer(instance=order, data = request.data)
+        serializer.is_vaild(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+
+    def destroy(self,request,pk=None):
+        order = Order.objects.get(id=pk)
+        order.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
